80c6217aa86a740ad70c3d8486afbab451db1f01
Wilson Foo Yu Kang <wilson.foo@customautosys.com>
2022-12-17 11:15:33 +0800

    Removed run-middleware as it was not working

--- dist/index.js
+++ dist/index.js
@@ -13,32 +13,74 @@ class DirectusTransportSafeRunMiddleware extends sdk_1.Transport {
         (0, run_middleware_1.default)(res.app);
     }
     request(method, path, data, options) {
-        let params = {
-            ip: '127.0.0.1',
-            method,
-            body: data,
-            headers: {
-                remoteAddress: '127.0.0.1',
-                origin: '127.0.0.1',
-                'content-type': 'application/json',
-                'transfer-encoding': 'identity'
-            },
-            connection: {},
-            original_req: this.req
-        };
-        if (options.params)
-            params.query = options.params;
-        if (options.headers)
-            Object.assign(params.headers, options.headers);
-        return new Promise(resolve => this.res.app.runMiddleware(this.url + path, params, (status, data, headers) => {
-            console.log(status, data, headers);
-            resolve({
-                headers,
-                raw: data,
-                data: data === null || data === void 0 ? void 0 : data.data,
-                status
+        return new Promise(resolve => {
+            let req = Object.assign({}, this.req);
+            let params = {
+                ip: '127.0.0.1',
+                method,
+                url: path,
+                body: data,
+                headers: {
+                    remoteAddress: '127.0.0.1',
+                    origin: '127.0.0.1',
+                    'content-type': 'application/json',
+                    'transfer-encoding': 'identity'
+                },
+                connection: {},
+                original_req: this.req
+            };
+            if (options.params)
+                params.query = options.params;
+            if (options.headers)
+                Object.assign(params.headers, options.headers);
+            Object.assign(req, params);
+            let headers = {};
+            let status = 200;
+            let res = Object.assign({}, this.res);
+            res.set = res.header = ((x, y) => {
+                if (x && typeof x === 'object') {
+                    for (let key in x) {
+                        res.setHeader(key, x[key]);
+                    }
+                }
+                else {
+                    res.setHeader(x, y !== null && y !== void 0 ? y : '');
+                }
+                return res;
             });
-        }));
+            res.setHeader = (x, y) => {
+                headers[x] = headers[x.toLowerCase()] = y;
+                return res;
+            };
+            res.getHeader = x => { var _a; return (_a = headers[x]) !== null && _a !== void 0 ? _a : null; };
+            res.redirect = ((code, url) => {
+                if (typeof code !== 'number' || (code > 0 && code < 600)) {
+                    status = typeof url === 'number' ? Math.max(1, Math.min(599, url)) : 301;
+                    url = code;
+                }
+                else {
+                    status = code;
+                }
+                res.setHeader("Location", url);
+                res.end();
+            });
+            res.status = number => {
+                status = number;
+                return res;
+            };
+            res.end = res.send = res.write = (raw => {
+                let response = {
+                    status,
+                    headers,
+                    raw
+                };
+                try {
+                    response.data = JSON.parse(raw);
+                }
+                catch (error) { }
+                resolve(response);
+            });
+        });
     }
 }
 exports.default = DirectusTransportSafeRunMiddleware;
--- src/index.ts
+++ src/index.ts
@@ -25,29 +25,72 @@ export default class DirectusTransportSafeRunMiddleware extends Transport{
 		data?:Record<string,any>,
 		options?:Omit<TransportOptions,'url'>
 	):Promise<TransportResponse<T,R>>{
-		let params:any={
-			ip:'127.0.0.1',
-			method,
-			body:data,
-			headers:{
-				remoteAddress:'127.0.0.1',
-				origin:'127.0.0.1',
-				'content-type':'application/json',
-				'transfer-encoding':'identity'
-			},
-			connection:{},
-			original_req:this.req
-		};
-		if(options.params)params.query=options.params;
-		if(options.headers)Object.assign(params.headers,options.headers);
-		return new Promise(resolve=>(this.res.app as any).runMiddleware(this.url+path,params,(status,data,headers)=>{
-			console.log(status,data,headers);
-			resolve({
-				headers,
-				raw:data,
-				data:data?.data,
-				status
-			});
-		}));
+		return new Promise(resolve=>{
+			let req=Object.assign({},this.req);
+			let params:any={
+				ip:'127.0.0.1',
+				method,
+				url:path,
+				body:data,
+				headers:{
+					remoteAddress:'127.0.0.1',
+					origin:'127.0.0.1',
+					'content-type':'application/json',
+					'transfer-encoding':'identity'
+				},
+				connection:{},
+				original_req:this.req
+			};
+			if(options.params)params.query=options.params;
+			if(options.headers)Object.assign(params.headers,options.headers);
+			Object.assign(req,params);
+			let headers={};
+			let status=200;
+			let res=Object.assign({},this.res);
+			res.set=res.header=((x,y)=>{
+				if(x&&typeof x==='object'){
+					for(let key in x){
+						res.setHeader(key,x[key]);
+					}
+				}else{
+					res.setHeader(x,y??'');
+				}
+				return res;
+			}) as any;
+			res.setHeader=(x,y)=>{
+				headers[x]=headers[x.toLowerCase()]=y;
+				return res;
+			};
+
+			res.getHeader=x=>headers[x]??null;
+
+			res.redirect=((code,url)=>{
+				if (typeof code!=='number'||(code>0&&code<600)) {
+					status=typeof url==='number'?Math.max(1,Math.min(599,url)):301;
+					url=code;
+				}else{
+					status=code;
+				}
+				res.setHeader("Location", url);
+				res.end();
+			}) as any;
+
+			res.status=number=>{
+				status=number;
+				return res;
+			};
+
+			res.end=res.send=res.write=(raw=>{
+				let response:any={
+					status,
+					headers,
+					raw
+				};
+				try{
+					response.data=JSON.parse(raw);
+				}catch(error){}
+				resolve(response);
+			}) as any;
+		});
 	}
 }
